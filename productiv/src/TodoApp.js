59734b5f42741418f952b905ba368604faf5bebf
Sara Heath <saramaheath@gmail.com>
2022-07-14 19:58:44 -0500

    update to editabletodolist and todoapp components

--- productiv/src/TodoApp.js
+++ productiv/src/TodoApp.js
@@ -15,49 +15,52 @@ import EditableTodoList from "./EditableTodoList";
  * App -> TodoApp -> { TodoForm, EditableTodoList }
  */
 
-function TodoApp({initialTodos}) {
+function TodoApp({ initialTodos }) {
   const [todos, setTodos] = useState(initialTodos);
-
 
   /** add a new todo to list */
   function create(newTodo) {
-    setTodos(todos => [...todos, newTodo]);
+    newTodo['id'] = uuid();
+    setTodos((todos) => [...todos, newTodo]);
   }
 
   /** update a todo with updatedTodo */
   function update(updatedTodo) {
-    setTodos(todos => [...todos, updatedTodo]);
+    setTodos((todos) => [...todos, updatedTodo]);
   }
 
   /** delete a todo by id */
   function remove(id) {
-   setTodos(todos => todos.filter(todo => todo.id !== id));
+    setTodos((todos) => todos.filter((todo) => todo.id !== id));
   }
-
+  
+  //TODO: look into handlesave func, currently defined in editabletodo
   return (
-      <main className="TodoApp">
-        <div className="row">
-
-          <div className="col-md-6">
-            <EditableTodoList /> OR
+    <main className="TodoApp">
+      <div className="row">
+        <div className="col-md-6">
+          {todos.length > 0 && (
+            <EditableTodoList todos={todos} update={update} remove={remove} />
+          )}
+          {todos.length === 0 && (
             <span className="text-muted">You have no todos.</span>
-          </div>
+          )}
+        </div>
 
+        {todos.length > 0 && (
           <div className="col-md-6">
-            (if no top todo, omit this whole section)
             <section className="mb-4">
               <h3>Top Todo</h3>
-              <TopTodo />
+              <TopTodo todos={todos} />
             </section>
-
             <section>
               <h3 className="mb-3">Add Nü</h3>
-              FIXME
+              <ToDoForm handleSave={handleSave} />
             </section>
           </div>
-
-        </div>
-      </main>
+        )}
+      </div>
+    </main>
   );
 }
 
